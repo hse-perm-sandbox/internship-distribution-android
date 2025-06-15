6a92db5d5433962875bdaaf96e5cf103796d6615
Danil <musikhin.danil@gmail.com>
2025-06-15 11:20:44 +0500

    Интерактивная скачка резюме

--- app/src/main/java/org/hse/appformlayout/StudentsTableActivity.java
+++ app/src/main/java/org/hse/appformlayout/StudentsTableActivity.java
@@ -33,6 +33,26 @@ public class StudentsTableActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(R.layout.spinner_dropdown_items);
         spinnerCVStatus.setAdapter(adapter);
 
+        // Добавляем обработку клика по строкам таблицы
+        for (int i = 1; i < tableStudents.getChildCount(); i++) { // пропускаем заголовок
+            if (tableStudents.getChildAt(i) instanceof android.widget.TableRow) {
+                android.widget.TableRow row = (android.widget.TableRow) tableStudents.getChildAt(i);
+                row.setOnClickListener(v -> {
+                    // Сбросить выделение у всех строк
+                    for (int j = 1; j < tableStudents.getChildCount(); j++) {
+                        if (tableStudents.getChildAt(j) instanceof android.widget.TableRow) {
+                            tableStudents.getChildAt(j).setBackgroundResource(0);
+                        }
+                    }
+                    // Выделить выбранную строку
+                    row.setBackgroundResource(R.color.lemon_yellow);
+                    downloadCVBtn.setEnabled(true);
+                    downloadCVBtn.setBackgroundResource(R.drawable.button_background);
+                    downloadCVBtn.setTextColor(getResources().getColor(android.R.color.white));
+                });
+            }
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
